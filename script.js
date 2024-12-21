f74c9a02bc4222c04074000f63519c50ab33c3c0
Dave-Abhishek <daveabhishek2004@gmail.com>
2024-12-21 16:19:12 +0530

    almost all done only some div and responsive left

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 function locomotiveAnimation() {
     gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
 }
 
 function loadingAnimation() {
@@ -92,14 +92,14 @@ function loadingAnimation() {
         display: "none",
     })
 
-    tl.from("#nav",{
+    tl.from("#nav", {
         opacity: 0,
     })
-    tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1", {
         y: 140,
         stagger: 0.2,
     })
-    tl.from("#hero1, #page2",{
+    tl.from("#hero1, #page2", {
         opacity: 0,
     }, "-=1.2") //this -> }, "-=1.2") <- will run this tl.from before others.
 
@@ -114,25 +114,111 @@ function loadingAnimation() {
 }
 
 function cursorAnimatoin() {
+    Shery.mouseFollower({
+        skew: true,
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 0.5, 
+    });
+    Shery.makeMagnet("#nav-part2 h4");
 
-    // Below is effect of one white corcle chasing cursor //
-    document.addEventListener("mousemove", function (dets) {
-        // console.log(dets.x)
-        gsap.to("#crsr", {
-            left: dets.x, // dets.x -> x coordinate
-            top: dets.y,  // dets.y -> y coordinate
-            // duration: 0.2, // this is to control speed of chasing but not necessary
+    // Below code is for video container that animate yellow circle following cursor inside video container
+    var videoContainer = document.querySelector("#video-container");
+    var video = document.querySelector("#video-container video");
+    videoContainer.addEventListener("mouseenter", function() {
+        // Language-Gujarati: jem jem mouse movement karse tem tem function run thase.
+        videoContainer.addEventListener("mousemove", function (dets) {
+            gsap.to(".mousefollower",{
+                opacity: 0,
+                //display none is not wirking I don't know why ?
+                // display: "none",
+            })
+            gsap.to("#video-cursor", {
+                left: dets.x - 490,
+                top: dets.y -300,
+            })
         })
     })
+    videoContainer.addEventListener("mouseleave", function() {
+        gsap.to(".mousefollower",{
+            opacity: 1,
+            // display: "initial",
+        })
+        gsap.to("#video-cursor", {
+            left: "70%",
+            top: "-15%",
+        })
+    })
+    var flag = 0;
+    videoContainer.addEventListener("click", function() {
+        if(flag == 0){
+            video.play();
+            video.style.opacity = 1;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-pause-mini-fill"></i>`
+            gsap.to("#video-cursor",{
+                scale:0.7,
+            })
+            flag = 1;
+        }
+        else{
+            video.pause();
+            video.style.opacity = 0;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-play-mini-fill"></i>`
+            gsap.to("#video-cursor",{
+                scale:1,
+            })
+            flag = 0;
+        }
+    })
+}
 
-
-    // Below is for using magnet effect using sheryjs. it is awailable on npmjs (website) -> sheryjs (search) //
-    Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/, {
-
-    });
+function page3Animation() {
+    Shery.imageEffect(".image-div", {
+        style: 5,
+        // debug: true, 
+        // //first run this debug then arrange according to you then click on save to clipboard then make config and paste.
+        config: { "a": { "value": 1.15, "range": [0, 30] }, "b": { "value": 1, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272749932567818 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": false }, "growSize": { "value": 2.18, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.09, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.46, "range": [0, 10] }, "metaball": { "value": 0.41, "range": [0, 2] }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.5, "range": [0, 2] }, "noise_scale": { "value": 10, "range": [0, 100] } },
+        gooey: true,
+    })
 }
 
 loadingAnimation()
-// cursorAnimatoin()
+cursorAnimatoin()
 locomotiveAnimation()
+page3Animation()
 
+document.addEventListener("mousemove",function (dets) {
+    gsap.to("#flag",{
+        x:dets.x,
+        y:dets.y,
+    })
+})
+document.querySelector("#hero3").addEventListener("mouseenter", function() {
+    gsap.to("#flag",{
+        opacity: 1,
+    })
+})
+document.querySelector("#hero3").addEventListener("mouseleave", function() {
+    gsap.to("#flag",{
+        opacity: 0,
+    })
+})
+
+//First Function to use cursor animation but then used shery js fot that.
+// function cursorAnimatoin() {
+
+//     // Below is effect of one white corcle chasing cursor //
+//     document.addEventListener("mousemove", function (dets) {
+//         // console.log(dets.x)
+//         gsap.to("#crsr", {
+//             left: dets.x, // dets.x -> x coordinate
+//             top: dets.y,  // dets.y -> y coordinate
+//             // duration: 0.2, // this is to control speed of chasing but not necessary
+//         })
+//     })
+
+
+//     // Below is for using magnet effect using sheryjs. it is awailable on npmjs (website) -> sheryjs (search) //
+//     Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/, {
+
+//     });
+// }
